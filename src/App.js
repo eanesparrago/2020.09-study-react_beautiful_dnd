6f6c80e56ec0142d5db178efc2873bd7dd0a6950
iansanwich <iansanwich@gmail.com>
2020-09-05 16:43:18 +0800

    Persist List Reordering with react-beautiful-dnd using the onDragEnd Callback

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { DragDropContext } from "react-beautiful-dnd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Column from "./Column";
+import initialData from "./initial-data";
+
+export class App extends Component {
+  state = initialData;
+
+  onDragEnd = (result) => {
+    /*
+    result
+    {
+      draggableId: "task-1",
+      type: "TYPE",
+      reason: "DROP",
+      source: {
+        droppableId: "column-1",
+        index: 0
+      },
+      destination: {
+        droppableId: "column-1",
+        index: 1
+      }
+    }
+    */
+
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+
+    // Sorting algorithm
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    this.setState(newState);
+  };
+
+  render() {
+    return (
+      <DragDropContext
+        // onDragStart
+        // onDragUpdate
+        onDragEnd={this.onDragEnd}
+      >
+        {this.state.columnOrder.map((columnId) => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(
+            (taskId) => this.state.tasks[taskId] // Uses lookup to map tasks in order
+          );
+
+          return (
+            <Column key={column.id} column={column} tasks={tasks}></Column>
+          );
+        })}
+      </DragDropContext>
+    );
+  }
 }
 
 export default App;
---
+++ src/Column.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Droppable } from "react-beautiful-dnd";
+
+import Task from "./Task";
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
+
+export class Column extends Component {
+  render() {
+    return (
+      <Container>
+        <Title>{this.props.column.title}</Title>
+
+        <Droppable droppableId={this.props.column.id}>
+          {(provided) => (
+            <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+              {this.props.tasks.map((task, index) => (
+                <Task key={task.id} index={index} task={task}></Task>
+              ))}
+
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
+      </Container>
+    );
+  }
+}
+
+export default Column;
